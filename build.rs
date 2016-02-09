d5678257bc6b8e18c833fb18a06fea6f290ebb79
Josh Matthews <josh@joshmatthews.net>
2016-02-09 04:24:23 -0500

    Initial commit.

---
+++ build.rs
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use std::process::Command;
+use std::env;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    assert!(Command::new("make")
+        .args(&["-R", "-f", "makefile.cargo", &format!("-j{}", env::var("NUM_JOBS").unwrap())])
+        .status()
+        .unwrap()
+        .success());
+    println!("cargo:rustc-flags=-L native={}", env::var("OUT_DIR").unwrap());
+}
+
