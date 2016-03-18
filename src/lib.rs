7bdb9f8d29c9ff5528d1316c70c7706ff17ca361
Daniel <ddefisher@gmail.com>
2016-03-18 01:37:05 -0400

    added basic ffi bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,51 @@
+extern crate libc;
+use libc::c_char;
+use libc::c_uchar;
+use libc::c_int;
+
 #[cfg(test)]
 mod test {
     #[test]
     fn it_works() {
     }
+}
+
+extern {
+
+    pub fn tinyfd_messageBox (
+        aTitle: *const c_char ,
+        aMessage: *const c_char,
+        aDialogType: *const c_char,
+        aIconType: *const c_char,
+        aDefaultButton: c_int) -> c_int;
+
+    pub fn tinyfd_inputBox (
+        aTitle: *const c_char ,
+        aMessage: *const c_char,
+        aDefaultInput: *const c_char) -> *const c_char;
+
+    pub fn tinyfd_saveFileDialog (
+        aTitle: *const c_char,
+        aDefaultPathAndFile: *const c_char,
+        aNumOfFilterPatterns: c_int,
+        aFilterPatterns: *mut *const c_char,
+        aSingleFilterDescription: *const c_char) -> *const c_char;
+
+    pub fn tinyfd_openFileDialog (
+        aTitle: *const c_char,
+        aDefaultPathAndFile: *const c_char,
+        aNumOfFilterPatterns: c_int,
+        aFilterPatterns: *mut *const c_char,
+        aSingleFilterDescription: *const c_char,
+        aAllowMultipleSelects: c_int) -> *const c_char;
+
+    pub fn tinyfd_selectFolderDialog (
+        aTitle: *const c_char,
+        aDefaultPath: *const c_char) -> *const c_char;
+
+    pub fn tinyfd_colorChooser (
+        aTitle: *const c_char,
+        aDefaultHexRGB: *const c_char,
+        aDefaultRGB: [c_uchar ; 3],
+        aoResultRGB: [c_uchar; 3]) -> *const c_char;
 }
