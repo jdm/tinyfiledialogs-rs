de1d1d339dd46866616176bffa5e1d90f85ce039
Bastien Orivel <eijebong@bananium.fr>
2018-03-21 00:31:37 +0100

    Replace the gcc crate by cc and bump version

--- build.rs
+++ build.rs
@@ -2,17 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-extern crate gcc;
+extern crate cc;
 
 use std::env;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
 
-    let mut cfg = gcc::Config::new();
-    cfg.file("libtinyfiledialogs/tinyfiledialogs.c");
-    cfg.flag("-v");
-    cfg.compile("libtinyfiledialogs.a");
+    cc::Build::new()
+        .file("libtinyfiledialogs/tinyfiledialogs.c")
+        .flag("-v")
+        .compile("libtinyfiledialogs.a");
 
     if target.contains("windows") {
         println!("cargo:rustc-link-lib=user32");
