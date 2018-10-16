a48b9b24620d22c0902c5e497afcebc2fd0d494d
Szabó Attila <neyo8826@gmail.com>
2018-10-16 05:34:57 +0200

    Better native upstream pull

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
-wget https://sourceforge.net/projects/tinyfiledialogs/files/tinyfiledialogs.h/download -O tinyfiledialogs.h
-tr -d '\r' < tinyfiledialogs.h > libtinyfiledialogs/tinyfiledialogs.h
-wget https://sourceforge.net/projects/tinyfiledialogs/files/tinyfiledialogs.c/download -O tinyfiledialogs.c
-tr -d '\r' < tinyfiledialogs.c > libtinyfiledialogs/tinyfiledialogs.c
+#!/bin/sh
+set -eux
+
+for ext in h c
+do
+	wget "https://sourceforge.net/projects/tinyfiledialogs/files/tinyfiledialogs.${ext}/download" -O - | tr -d '\r' > libtinyfiledialogs/tinyfiledialogs.${ext}
+done
+
+for p in $(ls patches -v)
+do
+	git apply patches/${p}
+done
