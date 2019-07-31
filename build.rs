cb1b1ac4771aa6376479d1a0d4d086ca58060473
nabijaczleweli <nabijaczleweli@gmail.com>
2019-07-31 14:57:37 +0200

    Also link to shell32 on Windows

--- build.rs
+++ build.rs
@@ -18,5 +18,6 @@ fn main() {
         println!("cargo:rustc-link-lib=user32");
         println!("cargo:rustc-link-lib=comdlg32");
         println!("cargo:rustc-link-lib=ole32");
+        println!("cargo:rustc-link-lib=shell32");
     }
 }
